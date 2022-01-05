a538fcc4e60df581e59cafe9340fb7232d033a32
ravi-kumar-1010 <ravikumartouby@gmail.com>
2022-01-05 15:13:12 +0530

    more on pydentic

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
 app = FastAPI()
 
 @app.get('/blog')
-def index(limit: int,published: bool):
+def index(limit:int = 10,published: bool=True,sort:Optional[str] = None):
     if published:
         for i in range(limit):
             yield {'Published item': i}
@@ -27,4 +31,22 @@ def blog_title(id: int, title: str):
 
 @app.get('/about')
 def about():
-    return {"About Me": "I am a developer"}
+    return {"About Me": "I am a developer"} 
+
+class Blog(BaseModel):
+    title: str
+    body:str
+    published_at:Optional[bool]
+    
+
+# @app.post('/blog/{info}')
+# def create_blog(info: str):
+#     return {"Blog": info}
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {"Blog": f"Blog is created with  {request.title} as title"}
+
+
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='127.0.0.1', port=9000)
